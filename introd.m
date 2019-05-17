11141de9bf63580e7a0d80255a239c7ea4792820
Lucas Tabelini Torres <lucastabelini@hotmail.com>
2019-05-17 11:10:05 -0300

    1 letra b

---
+++ introd.m
@@ -0,0 +1,16 @@
+% Introducao
+clc
+clear
+disp(' ');
+disp('Introducao ao Calculo Numerico usando o Octave/Matlab'); 
+disp('Alunos: Lucas Tabelini Torres & Luiz Felipe Boina');
+disp('Prof. Andrea Valli');
+disp(' ');
+disp('Programa:');
+disp('1. Sistemas Lineares - 1');
+disp('2. Sistemas Lineares - 2');
+disp('3. Ajuste de Curvas - 1');
+disp('4. Ajuste de Curvas - 2');
+
+
+
---
+++ main.m
@@ -0,0 +1,47 @@
+clc
+clear
+clear functions
+%
+% -----------------------------------------------------------
+% 	Introducao ao Calculo Numerico usando o Octave/Matlab		              
+%             Prof. Andrea Valli
+%------------------------------------------------------------
+% Descricao do Trabalho 
+% -----------------------------------------------------------
+introd
+disp(' ');
+disp('Tecle alguma coisa para continuar...'); pause
+%
+% -----------------------------------------------------------
+% Sistemas Lineares - 1
+% -----------------------------------------------------------
+sl1
+disp(' ');
+disp('Tecle alguma coisa para continuar...'); pause
+%
+% -----------------------------------------------------------
+% Sistemas Lineares - 2
+% -----------------------------------------------------------
+sl2
+disp(' ');
+disp('Tecle alguma coisa para continuar...'); pause
+%
+% -----------------------------------------------------------
+% Ajuste de Curvas - 1
+% -----------------------------------------------------------
+ac1
+disp(' ');
+disp('Tecle alguma coisa para continuar...'); pause
+% -----------------------------------------------------------
+% Ajuste de Curvas - 2
+% -----------------------------------------------------------
+ac2
+disp(' ');
+disp('Tecle alguma coisa para continuar...'); pause
+%
+% -----------------------------------------------------------
+% Final da Apresentacao
+% -----------------------------------------------------------
+disp(' ');
+disp('Tecle alguma coisa para terminar a apresentacao...'); pause
+clc
